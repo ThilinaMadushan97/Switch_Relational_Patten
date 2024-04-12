1ba97bf06233859fd0846ce26f5ae284f1956be9
dell <you@example.com>
2024-04-12 21:00:58 +0530

    Add project files.

---
+++ SwitchRelationalPatten/Program.cs
@@ -0,0 +1,13 @@
+﻿List<int> numbers = new List<int> { 10, -2, 31, 5, 0, 22 };
+
+foreach (int num in numbers)
+{
+    var result = num switch
+    {
+        > 0 => $"{num} is a positive number",
+         0 => $"Zero",
+        < 0 => $"{num} is a negative number"
+
+    };
+    Console.WriteLine(result);
+}
